0f7ac6fba1478ea3c15ba8b37c512ecc6b86284a
nitoroto <161154942+nitoroto@users.noreply.github.com>
2024-06-25 23:51:31 +0200

    update error catching

--- fetchImage.js
+++ fetchImage.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const axios = require('axios');
-require('dotenv').config(); 
+require('dotenv').config();
+
 const config = {
   imageURL: process.env.IMAGE_URL || 'default_image_url',
 };
+
 const downloadImage = async (url, path) => {
   try {
     const response = await axios({
@@ -11,10 +13,22 @@ const downloadImage = async (url, path) => {
       url: url,
       responseType: 'stream', 
     });
-    response.data.pipe(fs.createWriteStream(path));
-    console.log(`Image saved to ${path}`);
+
+    const writer = fs.createWriteStream(path);
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on('finish', () => {
+        console.log(`Image saved to ${path}`);
+        resolve(); // Resolve the promise after successfully writing the file.
+      });
+      writer.on('error', reject); // Reject the promise if an error occurs while writing the file.
+    });
+
   } catch (error) {
     console.error('Error downloading image:', error);
   }
-}
+};
+
 downloadImage(config.imageURL, 'data/downloadedImage.jpg');
