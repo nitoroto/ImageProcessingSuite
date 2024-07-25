6ece8fc02bc3dfd464e4eb0d881bac4142705931
nitoroto <161154942+nitoroto@users.noreply.github.com>
2024-07-25 08:25:37 +0200

    implemented function

--- utils/logger.js
+++ utils/logger.js
@@ -9,7 +9,20 @@ const logLevels = {
 function log(message, level) {
   try {
     if (process.env.LOG_LEVEL && Object.values(logLevels).indexOf(level) >= Object.values(logLevels).indexOf(process.env.LOG_LEVEL)) {
-      console.log(`${new Date().toISOString()} [${level}]: ${message}`);
+      const timestamp = new Date().toISOString();
+      const formattedMessage = `${timestamp} [${level}]: ${message}`;
+
+      switch (level) {
+        case logLevels.warn:
+          console.warn(`\x1b[33m${formattedMessage}\x1b[0m`); // Yellow color
+          break;
+        case logLevels.error:
+          console.error(`\x1b[31m${formattedMessage}\x1b[0m`); // Red color
+          break;
+        default:
+          // Info or any other level not explicitly handled will be in plain text
+          console.log(formattedTriumph);
+      }
     }
   } catch (error) {
     console.error(`${new Date().toISOString()} [ERROR]: Failed to log message. Original message: ${message}`, error);
